cb4bb8e7783f04af7c27999f293098c35b02b898
Alex Wensley <cwensley@gmail.com>
2019-12-02 18:07:42 -0700

    Updates

---
+++ precache-manifest.2bdea3fbd54ac9f8a076d959b9e4153c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caed636642f4f324b5003b5357f1a787",
+    "url": "/moviegallery/index.html"
+  },
+  {
+    "revision": "959d323e8678ffe45621",
+    "url": "/moviegallery/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8ee15d55990edc69b344",
+    "url": "/moviegallery/static/js/2.38fe95f2.chunk.js"
+  },
+  {
+    "revision": "959d323e8678ffe45621",
+    "url": "/moviegallery/static/js/main.e6d2b724.chunk.js"
+  },
+  {
+    "revision": "030281ed9429e3c26901",
+    "url": "/moviegallery/static/js/runtime-main.282e8574.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/moviegallery/static/media/logo.25bf045c.svg"
+  }
+]);
