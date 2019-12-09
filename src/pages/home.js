9800419427a15e54107dcb7cc84cd0f8c0b7296b
Alex Wensley <cwensley@gmail.com>
2019-12-08 18:53:20 -0700

    added page/results selectors, working on making them change with user selections

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,10 +6,14 @@ import PageTitle from "../atoms/pageTitle";
 const Home = () => {
     const [moviestoDisplay, setmoviestoDisplay] = useState("");
     const [userInput,setUserInput]=useState(localStorage.getItem(`searchString`));
-    const [PageSelected,setPageSelected]=useState(1);
+    const [PageSelected,setPageSelected]=useState(localStorage.getItem('PageSelected'));
+    const PageChoices=[1,2,3,4,5];
+    const [ResultsSelected,setResultsSelected]=useState(10);
+    const ResultsDisplayed=[10];
 
     async function getmovieData() {
         localStorage.setItem(`searchString`,userInput);
+        localStorage.setItem('PageSelected',PageSelected);
         const rawData = await fetch(`https://www.omdbapi.com/?s=${userInput}&page=${PageSelected}&apikey=${movieAPI}`)
             .then(response => response.json());
         setmoviestoDisplay(rawData);
@@ -24,7 +28,7 @@ const Home = () => {
             <input type="text" size="15" className="h6" onChange={event=>setUserInput(event.target.value)}
                    onKeyPress={event=>{if (event.key === "Enter") {getmovieData()}}}/>
             <button type="submit" value="Submit" className="btn btn-primary btn-sm"
-                    onClick={()=>getmovieData()}>Title Search</button>
+                    onClick={()=>{setPageSelected(1);getmovieData()}}>Title Search</button>
             <br/>
             <h2 className="text-warning">No Searches Matching: {localStorage.getItem(`searchString`)}</h2>
         </div>
@@ -36,18 +40,20 @@ const Home = () => {
                     <input type="text" size="15" className="h6" onChange={event=>setUserInput(event.target.value)}
                            onKeyPress={event=>{if (event.key === "Enter") {getmovieData()}}}/>
                     <button type="submit" value="Submit" className="btn btn-primary btn-sm"
-                            onClick={()=>getmovieData()}>Title Search</button>
+                                onClick={()=>{setPageSelected(1);getmovieData()}}>Title Search</button>
                 </div>
                 <div className="col-6 text-left">
                     <p className="text-warning">Total Search Results: {moviestoDisplay.totalResults}</p>
-                    <p className="text-warning">Total Pages: {Math.trunc(moviestoDisplay.totalResults/10)+1}</p>
-                    <p className="text-warning">Page Displayed: {PageSelected}</p>
-                    <label className="text-warning"> Page Selector__
-                        <select onChange={event => setPageSelected(event.target.value)}>
-                            <option id={1} value={1}>Page 1</option>
-                            <option id={2} value={2}>Page 2</option>
-                            <option id={3} value={3}>Page 3</option>
-                            <option id={4} value={4}>Page 4</option>
+                    <p className="text-warning">Total Pages: {Math.trunc(moviestoDisplay.totalResults/ResultsSelected)+1}</p>
+                    <label className="text-warning"> Results per Page:
+                        <select className="ml-2" value={ResultsSelected} onChange={event => setResultsSelected(event.target.value)}>
+                            {ResultsDisplayed.map(item=>(<option key={item} value={item}>{item}</option>))}
+                        </select>
+                    </label>
+                    <br/>
+                    <label className="text-warning"> Page Selector:
+                        <select className="ml-2" value={PageSelected} onChange={event => setPageSelected(event.target.value)}>
+                            {PageChoices.map(item=>(<option key={item} value={item}>{item}</option>))}
                         </select>
                     </label>
                     <p className="text-warning">Searched Phrase: {localStorage.getItem(`searchString`)}</p>
