006a435daaf762f0fed8dd1cc1f3d8fa4cb31e10
Alex Wensley <cwensley@gmail.com>
2019-12-08 17:23:02 -0700

    made the navigation bar collapsable for small viewing screens

--- src/organisms/MainNavBar.js
+++ src/organisms/MainNavBar.js
@@ -3,14 +3,26 @@ import React from "react";
 
 const MainNavBar=()=> {
     return (
-            <nav className="bg-dark navbar navbar-expand o-mainnavbar_padding fixed-top p-1">
-                <h2 className="text-light">Movie Gallery</h2>
-                <div className="ml-auto">
-                    <Link to="/" className="navbar-brand px-3 py-1">Search</Link>
-                    <Link to="/favorites" className="navbar-brand px-3 py-1">Favorites</Link>
-                    <Link to="/details" className="navbar-brand px-3 py-1">Details</Link>
-                </div>
-            </nav>
+        <nav className="navbar navbar-expand-md bg-dark fixed-top p-1">
+            <h2 className="text-light">Movie Gallery</h2>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span className="text-white"><i className="fas fa-bars"></i></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                    <li className="navbar-brand">
+                        <Link to="/" className="nav-link px-3 py-1">Search</Link>
+                    </li>
+                    <li className="navbar-brand">
+                        <Link to="/favorites" className="nav-link px-3 py-1">Favorites</Link>
+                    </li>
+                    <li className="navbar-brand">
+                        <Link to="/details" className="nav-link px-3 py-1">Details</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 };
 
